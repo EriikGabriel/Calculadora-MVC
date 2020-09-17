f9e07b89187e16b64a3bd37c30ce12892892c7b1
EriikGabriel <erikgabriel.lins@hotmail.com>
2020-09-17 13:44:28 -0300

    Commit - Configurando o SW

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = "erik-cache-v1";
+var urlsToCache = [
+  "css/style.css",
+  "css/bootstrap.css",
+  "images/icon.ico",
+  "js/jquery-3.5.1.min.js",
+  "js/bootstrap.js",
+  "libs/fontawesome/all.min.css",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Cache Aberto!");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWidth(
+    caches.match(event.request).then(function (response) {
+      if (response) return response;
+      return fetch;
+    })
+  );
+});
