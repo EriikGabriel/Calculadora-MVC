6b9cdd8d911f500896b8a53183c7deb4ae2a51ac
EriikGabriel <erikgabriel.lins@hotmail.com>
2020-10-08 17:11:50 -0300

    Commit - Atualizando o SW

--- sw.js
+++ sw.js
@@ -21,7 +21,36 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       if (response) return response;
-      return fetch;
+
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener("active", function (event) {
+  var cacheAllowList = ["erik-cache-v1", "erik-cache-v2"];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheAllowList.indexOf(cacheName) == -1) return cache.delete(cacheName);
+        })
+      );
     })
   );
 });
