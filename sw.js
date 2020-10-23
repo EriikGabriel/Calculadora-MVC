f2ba3defd349f96976f40842df5f6fb25e751f48
EriikGabriel <erikgabriel.lins@hotmail.com>
2020-10-22 23:37:43 -0300

    Commit - Atualizando PWA Builder

--- sw.js
+++ sw.js
@@ -1,56 +1,20 @@
-var CACHE_NAME = "erik-cache-v1";
-var urlsToCache = [
-  "css/style.css",
-  "css/bootstrap.css",
-  "images/icon.ico",
-  "js/jquery-3.5.1.min.js",
-  "js/bootstrap.js",
-  "libs/fontawesome/all.min.css",
+var cacheName = 'erik-v1';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
 ];
-
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Cache Aberto!");
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
     })
   );
 });
-
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) return response;
-
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(function (response) {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      });
-    })
-  );
-});
-
-self.addEventListener("active", function (event) {
-  var cacheAllowList = ["erik-cache-v1", "erik-cache-v2"];
-
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheAllowList.indexOf(cacheName) == -1) return cache.delete(cacheName);
-        })
-      );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
